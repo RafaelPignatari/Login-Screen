07afc78f7407813e177406e4d1bd29a79258b608
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2021-10-12 19:38:48 -0300

    Add project files.

---
+++ TelaN2B2/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelaN2B2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+        public IActionResult Cadastrar()
+        {
+            return View("Cadastrar");
+        }
+    }
+}
---
+++ TelaN2B2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TelaN2B2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
